9856f33317f1b21efaabae04db4b80230adebf41
Bety Rostandy <42425634+brostandy@users.noreply.github.com>
2019-03-26 22:10:43 -0400

    Update mineRva_Hcanadensis.R

--- src/mineRva_Hcanadensis.R
+++ src/mineRva_Hcanadensis.R
@@ -9,8 +9,8 @@ data<-read.csv("data/180517_Sample_pos_neg_sn10.csv",check.names="FALSE")
 sample_group=sgroup(data,c("MeOH","03125","0625","125","250","500"))
 dataset<-dataSet(data,sample_group,rt="rtmed",mz="mzmed")
 x_method1<-xM1(dataset,sample_group)
-msr<-msr(xm1=x_method1,sample_group)
-outliers<-exclude5(xm1=x_method1,msr,T=155) #optional
+msr<-msr(xm1=x_method1,sample_group) 
+outliers<-exclude5(xm1=x_method1,msr,rtgrp=155) #optional
 extract_ions<-ionx(xm1=x_method1,sgrp=sample_group,rtgrp=155,a=0.8) #OPTIONAL: ,exclude=outliers
 x_method2<-xM2(x=dataset,sample_group)
 relate_ions<-ionr(xm2=x_method2,inx=extract_ions,sample_group,rtgrp=155, a=0.8) #OPTIONAL: ,exclude=outliers
